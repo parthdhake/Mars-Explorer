0709326bc15afb7653abf45a96d624b3927641af
parth <parthdhake@gmail.com>
2021-02-15 19:51:39 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/mars-explorer'));
+app.use(express.static(__dirname + '/dist/src/mars-explorer'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/mars-explorer/index.html'));});
+'/dist/mars-explorer/src/index.html'));});
 app.listen(process.env.PORT || 8080);
